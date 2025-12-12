1745986a8f551ad113488bfb1387696f1f5c21c9
Shatokhin Vitaliy <amfib87@gmail.com>
2025-12-12 19:31:24 +0300

    iter5FirstCommit

--- internal/config/flags.go
+++ internal/config/flags.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"flag"
+	"os"
 )
 
 type Cnfg struct {
@@ -19,8 +20,15 @@ func NewConfig() *Cnfg {
 // parseFlags обрабатывает аргументы командной строки
 // и сохраняет их значения в соответствующих переменных
 func ParseFlags(cfg *Cnfg) {
-	flag.StringVar(&cfg.ServRunAddr, "a", ":8080", "address and port to run server")
+	flag.StringVar(&cfg.ServRunAddr, "a", "", "address and port to run server")
 	flag.StringVar(&cfg.AddrForURL, "b", "", "address to short URL")
 	// парсим переданные серверу аргументы в зарегистрированные переменные
 	flag.Parse()
+
+	if envServAddr := os.Getenv("SERVER_ADDRESS"); envServAddr != "" {
+		cfg.ServRunAddr = envServAddr
+	}
+	if envAddrForURL := os.Getenv("BASE_URL"); envAddrForURL != "" {
+		cfg.AddrForURL = envAddrForURL
+	}
 }
